731fca01bf36921a8ad72857fc3b3920ce22e4b2
Mikele91 <michele.amoruso91.8@gmail.com>
2021-10-19 16:17:01 +0200

    aggiunti numeri random per punteggio

--- snack1/snack1.php
+++ snack1/snack1.php
@@ -7,21 +7,21 @@ $tappa1=[
 
     [
         "casa" => "Bisceglie",
-        "puntCasa" => "60",
+        // "puntCasa" => "60",
         "trasferta" => "Milano",
-        "puntTrasferta" => "40",
+        // "puntTrasferta" => "40",
     ],
     [
         "casa" => "Torino",
-        "puntCasa" => "10",
+        // "puntCasa" => "10",
         "trasferta" => "Firenze",
-        "puntTrasferta" => "20",
+        // "puntTrasferta" => "20",
     ],
     [
         "casa" => "Roma",
-        "puntCasa" => "90",
+        // "puntCasa" => "90",
         "trasferta" => "Napoli",
-        "puntTrasferta" => "20",
+        // "puntTrasferta" => "20",
     ],
 
 ];
@@ -30,8 +30,8 @@ $tappa1=[
 for( $i = 0; $i< count($tappa1); $i++){
     echo "  {$tappa1[$i]["casa"]} ";
     echo " vs {$tappa1[$i]["trasferta"]} ";
-    echo "punteggio: {$tappa1[$i]["puntCasa"]} -";
-    echo " {$tappa1[$i]["puntTrasferta"]} ";
+    echo "punteggio: " . rand(1,100)  ;
+    echo  " - " .  rand(1,100);
 }
 
 
